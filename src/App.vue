00e9a2ad6a09f4d7027fed298aa4be02abb803c4
Vladislav Harchenko <vladislav.harchenko2017@yandex.ru>
2024-09-29 00:17:21 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<script setup>
+import { ref } from "vue";
+import VueHeader from "./widgets/header.vue";
+let count = ref(0);
+function counter() {
+  count.value++;
+}
+</script>
+
+<template>
+  <h1 class="text-3xl font-bold underline">Hello world!</h1>
+  <VueHeader />
+  <div class="welcome">
+    <h1>Welcome</h1>
+    <div class="hello">hello world</div>
+    <p>{{ count }}</p>
+    <button @click="counter">Счетчик</button>
+  </div>
+</template>
+
+<style scoped>
+.welcome {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+.hello {
+  font-size: 26px;
+}
+</style>
---
+++ src/widgets/header.vue
@@ -0,0 +1,11 @@
+<script></script>
+<template>
+  <h1>rr</h1>
+  <header>header</header>
+</template>
+
+<style scoped>
+h1 {
+  color: red;
+}
+</style>
