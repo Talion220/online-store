b2506d6572289cc8b396293e544ad20b207323f5
Vladislav Harchenko <vladislav.harchenko2017@yandex.ru>
2024-10-06 01:02:09 +0700

    structure upd

--- src/App.vue
+++ src/App.vue
@@ -1,41 +1,9 @@
 <script setup>
-import { ref, onMounted } from "vue";
-import VueHeader from "./widgets/header.vue";
-let count = ref(0);
-let arr = ref([1, 2, 3, 4, 5]);
-function counter() {
-  count.value++;
-  arr.value = [1, 1, 1, 1, 1];
-}
-let res = ref(null);
-setTimeout(() => {
-  fetch("https://jsonplaceholder.typicode.com/todos/1", {
-    method: "GET",
-  })
-    .then((response) => (res.value = response.json()))
-    .then((response) => (res.value = response));
-}, 1000);
+import Home from "./pages/Home.vue";
 </script>
 
 <template>
-  <h1 class="text-3xl font-bold underline">Hello world!</h1>
-  <VueHeader v-for="item in 3" :key="item" title="Заголовок" header="h1" />
-  <div class="welcome">
-    <h1>Welcome</h1>
-    <div class="hello">hello world</div>
-    <p>{{ count }}</p>
-    <p>{{ res }}</p>
-    <button @click="counter">Счетчик</button>
-  </div>
+  <Home />
 </template>
 
-<style scoped>
-.welcome {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-}
-.hello {
-  font-size: 26px;
-}
-</style>
+<style scoped></style>
